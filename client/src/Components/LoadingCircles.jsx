e6459288cb6bc6906a73f9f350137533115e4cc3
Anandaram R <anandaramr1915@gmail.com>
2024-08-19 00:02:49 +0530

    Create loading component

---
+++ client/src/Components/LoadingCircles.jsx
@@ -0,0 +1,39 @@
+
+export default function LoadingCircles() {
+    return (
+        <div className="h-[20svh]">
+            <svg className="h-[10svh] justify-self-end">
+                    <circle cy="50%" cx="40%" r="10" fill="skyblue" >
+                        <animate
+                            attributeName="opacity"
+                            begin="0s"
+                            dur="0.9s"
+                            from="0"
+                            to="1"
+                            repeatCount="indefinite"
+                        />
+                    </circle>
+                    <circle cy="50%" cx="50%" r="10" fill="skyblue" >
+                        <animate
+                            attributeName="opacity"
+                            begin="0.3s"
+                            dur="0.9s"
+                            from="0"
+                            to="1"
+                            repeatCount="indefinite"
+                        />
+                    </circle>
+                    <circle cy="50%" cx="60%" r="10" fill="skyblue" >
+                        <animate
+                            attributeName="opacity"
+                            begin="0.6s"
+                            dur="0.9s"
+                            from="0"
+                            to="1"
+                            repeatCount="indefinite"
+                        />
+                    </circle>
+            </svg>
+        </div>
+    )
+}
